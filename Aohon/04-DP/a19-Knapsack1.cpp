3861e27444a13c854b7b41582667d355d0c8af5c
tm-hack <takufrom1124@gmail.com>
2023-03-10 01:10:35 +0900

    done a19

--- Aohon/04-DP/a19-Knapsack1.cpp
+++ Aohon/04-DP/a19-Knapsack1.cpp
@@ -5,25 +5,35 @@
 using namespace std;
 using namespace atcoder;
 
-#define MAX_N 200'009
+#define FALSE_V -1'000'000'000'000'000LL
 
-int N, S, A[MAX_N];
-bool dp[69][MAX_N];
+long long N, W, w[109], v[109];
+long long dp[109][100009];
 
 int main() {
     // 入力
-    int N;
-    cin >> N;
-    for (int i = 1; i <= N; i++) cin >> A[i];
-
-    pair<int, int> score[MAX_N];
-    for (int i = 1; i <= N; i++) {
-        cin >> score[i].first;
-        score[i].second = i;
+    cin >> N >> W;
+    for (int i = 1; i <= N; i++) cin >> w[i] >> v[i];
+    for (int i = 0; i <= N; i++) {
+        for (int j = 0; j <= W; j++) {
+            dp[i][j] = FALSE_V;
+        }
     }
-    sort(score, score + MAX_N, greater<pair<int, int>>());
+
+    // 動的計画法
+    dp[0][0] = 0;
+    for (int i = 1; i <= N; i++) {
+        for (int j = 0; j <= W; j++) {
+            if (j < w[i])
+                dp[i][j] = dp[i - 1][j];
+            else
+                dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - w[i]] + v[i]);
+        }
+    }
 
     // 出力
-    cout << "Good Luck!" << endl;
+    long long Answer = 0;
+    for (int i = 0; i <= W; i++) Answer = max(Answer, dp[N][i]);
+    cout << Answer << endl;
     return 0;
 }
