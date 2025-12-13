dfd549c04e058b5ef6229c2a35679c259bc7e26d
Takumi Masuda <takufrom1124@gmail.com>
2025-12-14 00:18:53 +0900

    fix

---
+++ _template/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var wtr = bufio.NewWriter(os.Stdout)
+
+func main() {
+
+}
+
+// ==================================================
+// init
+// ==================================================
+func init() {
+	sc.Split(bufio.ScanWords)
+}
+
+// ==================================================
+// io
+// ==================================================
+func nextInt() int {
+	sc.Scan()
+	i, _ := strconv.Atoi(sc.Text())
+	return i
+}
+
+func nextString() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+// ==================================================
+// matrix
+// ==================================================
+func matrix2D(h, w, val int) [][]int {
+	res := make([][]int, h)
+	for i := 0; i < h; i++ {
+		res[i] = make([]int, w)
+		if val != 0 {
+			for j := 0; j < w; j++ {
+				res[i][j] = val
+			}
+		}
+	}
+	return res
+}
