ef2c61e4c3fdf4010578462d2bf416e05892c3f4
Takumi Masuda <takufrom1124@gmail.com>
2025-12-22 23:39:25 +0900

    ABC436 a

--- _result/_ABC436/a/main.go
+++ _result/_ABC436/a/main.go
@@ -2,23 +2,29 @@ package main
 
 import (
 	"bufio"
+	"cmp"
 	"fmt"
 	"os"
 	"strconv"
-	"strings"
 )
 
 var sc = bufio.NewScanner(os.Stdin)
 var wtr = bufio.NewWriter(os.Stdout)
 
 func main() {
+	defer flush()
 	N := nextInt()
 	S := nextString()
 
-	prefix := strings.Repeat("o", N-len(S))
-	result := prefix + S
+	buf := make([]byte, 0, N)
+	for i := 0; i < N-len(S); i++ {
+		buf = append(buf, 'o')
+	}
 
-	fmt.Println(result)
+	buf = append(buf, S...)
+
+	res := string(buf)
+	out(res)
 }
 
 // ==================================================
@@ -26,6 +32,16 @@ func main() {
 // ==================================================
 func init() {
 	sc.Split(bufio.ScanWords)
+}
+
+// ==================================================
+// num
+// ==================================================
+func abs(a int) int {
+	if a > 0 {
+		return a
+	}
+	return -a
 }
 
 // ==================================================
@@ -40,4 +56,91 @@ func nextInt() int {
 func nextString() string {
 	sc.Scan()
 	return sc.Text()
+}
+
+func out(v ...interface{}) {
+	fmt.Fprintln(wtr, v...)
+}
+
+func flush() {
+	e := wtr.Flush()
+	if e != nil {
+		panic(e)
+	}
+}
+
+// ==================================================
+// matrix
+// ==================================================
+func matrix2D[T any](h int, w int, val T) [][]T {
+	res := make([][]T, h)
+	for i := 0; i < h; i++ {
+		res[i] = make([]T, w)
+		for j := 0; j < w; j++ {
+			res[i][j] = val
+		}
+	}
+	return res
+}
+
+// ==================================================
+// dp
+// ==================================================
+func chmin[T cmp.Ordered](a *T, b T) bool {
+	if *a > b {
+		*a = b
+		return true
+	}
+	return false
+}
+
+func chmax[T cmp.Ordered](a *T, b T) bool {
+	if *a < b {
+		*a = b
+		return true
+	}
+	return false
+}
+
+// ==================================================
+// binarySearch
+// ==================================================
+func binarySearch(ok, ng int, f func(int) bool) int {
+	if !f(ok) {
+		return -1
+	}
+	if f(ng) {
+		return ng
+	}
+	for abs(ok-ng) > 1 {
+		mid := (ok + ng) / 2
+
+		if f(mid) {
+			ok = mid
+		} else {
+			ng = mid
+		}
+	}
+
+	return ok
+}
+
+func lowerBound(v int, sl []int) int {
+	if len(sl) == 0 {
+		return -1
+	}
+	idx := binarySearch(0, len(sl)-1, func(c int) bool {
+		return sl[c] <= v
+	})
+	return idx
+}
+
+func upperBound(v int, sl []int) int {
+	if len(sl) == 0 {
+		return 0
+	}
+	idx := binarySearch(0, len(sl)-1, func(c int) bool {
+		return sl[c] <= v
+	})
+	return idx + 1
 }
